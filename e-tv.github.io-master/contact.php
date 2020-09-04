1537464204b3eb9de77be8b5f364a6b4168b1bed
E-TV <tvelectronicstv@gmail.com>
2020-09-04 12:27:01 +0200

    Initial commit

---
+++ e-tv.github.io-master/contact.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>12V Portable Powerbank</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body class="is-preload">
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+
+							<!-- Header -->
+								<header id="header">
+									  <p href="index.html" style="color:black;font-size:30px;">12V Portable Powerbank</p>
+									<ul class="icons">
+										<li><a href="https://github.com/E-TV" class="icon brands fa-github"><span class="label">YouTube</span></a></li>
+										<li><a href="https://www.youtube.com/channel/UCVW9AqTwOfjLOtqSVVc8O8A" class="icon brands fa-youtube"><span class="label">Github</span></a></li>
+										
+										
+										<!--<li><a href="#" class="icon brands info-circle"><span class="label">Instructables</span></a></li>
+										<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+										<li><a href="#" class="icon brands fa-medium-m"><span class="label">Medium</span></a></li>-->
+									</ul>
+								</header>
+
+						
+								<form>
+								<div class="fields">
+									<div class="field half">
+										<input type="text" name="name" id="name" placeholder="Name" />
+									</div>
+									<div class="field half">
+										<input type="email" name="email" id="email" placeholder="Email" />
+									</div>
+									<div class="field">
+										<textarea name="message" id="message" placeholder="Message" rows="7"></textarea>
+									</div>
+								</div>
+								<ul class="actions">
+									<li><input type="submit" value="Send Message" class="button primary" /></li>
+								</ul>
+							</form>
+							
+
+						</div>
+					</div>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>	
